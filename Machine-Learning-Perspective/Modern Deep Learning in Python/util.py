e4b8b47c792f00a2b7ddbb4b5a36e79e508e7b76
Nate <ndake11@gmail.com>
2018-03-26 16:58:28 -0600

    finished intro and started gradient descent specifics

---
+++ Machine-Learning-Perspective/Modern Deep Learning in Python/util.py
@@ -0,0 +1,301 @@
+
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
+
+
+def get_clouds():
+    Nclass = 500
+    D = 2
+
+    X1 = np.random.randn(Nclass, D) + np.array([0, -2])
+    X2 = np.random.randn(Nclass, D) + np.array([2, 2])
+    X3 = np.random.randn(Nclass, D) + np.array([-2, 2])
+    X = np.vstack([X1, X2, X3])
+
+    Y = np.array([0]*Nclass + [1]*Nclass + [2]*Nclass)
+    return X, Y
+
+
+def get_spiral():
+    # Idea: radius -> low...high
+    #           (don't start at 0, otherwise points will be "mushed" at origin)
+    #       angle = low...high proportional to radius
+    #               [0, 2pi/6, 4pi/6, ..., 10pi/6] --> [pi/2, pi/3 + pi/2, ..., ]
+    # x = rcos(theta), y = rsin(theta) as usual
+
+    radius = np.linspace(1, 10, 100)
+    thetas = np.empty((6, 100))
+    for i in range(6):
+        start_angle = np.pi*i / 3.0
+        end_angle = start_angle + np.pi / 2
+        points = np.linspace(start_angle, end_angle, 100)
+        thetas[i] = points
+
+    # convert into cartesian coordinates
+    x1 = np.empty((6, 100))
+    x2 = np.empty((6, 100))
+    for i in range(6):
+        x1[i] = radius * np.cos(thetas[i])
+        x2[i] = radius * np.sin(thetas[i])
+
+    # inputs
+    X = np.empty((600, 2))
+    X[:,0] = x1.flatten()
+    X[:,1] = x2.flatten()
+
+    # add noise
+    X += np.random.randn(600, 2)*0.5
+
+    # targets
+    Y = np.array([0]*100 + [1]*100 + [0]*100 + [1]*100 + [0]*100 + [1]*100)
+    return X, Y
+
+
+"""
+Read in the data, transform using PCA, and return the pca model which contains the transformation matrix, 
+and the mean of x which is subtracted from x before doing PCA
+"""
+def get_transformed_data():
+    print("Reading in and transforming data...")
+
+    df = pd.read_csv('../../../data/MNIST/train.csv')
+    data = df.as_matrix().astype(np.float32)
+    np.random.shuffle(data)
+
+    X = data[:, 1:]
+    mu = X.mean(axis=0)
+    X = X - mu # center the data
+    pca = PCA()
+    Z = pca.fit_transform(X)
+    Y = data[:, 0].astype(np.int32)
+
+    plot_cumulative_variance(pca)
+
+    return Z, Y, pca, mu
+
+"""
+Takes in raw X, subtracts mean, and divides by standard deviation
+"""
+def get_normalized_data():
+    print("Reading in and transforming data...")
+
+    df = pd.read_csv('../../../data/MNIST/train.csv')
+    data = df.as_matrix().astype(np.float32)
+    np.random.shuffle(data)
+    X = data[:, 1:]
+    mu = X.mean(axis=0)
+    std = X.std(axis=0)
+    np.place(std, std == 0, 1)
+    X = (X - mu) / std # normalize the data
+    Y = data[:, 0]
+    return X, Y
+
+"""
+Function plot the cumulative variance, after doing PCA. This justifies taking the top 300 principle
+components because they contain over 95% of the variance of the original data
+"""
+def plot_cumulative_variance(pca):
+    P = []
+    for p in pca.explained_variance_ratio_:
+        if len(P) == 0:
+            P.append(p)
+        else:
+            P.append(p + P[-1])
+    plt.plot(P)
+    plt.show()
+    return P
+
+"""
+Function that performs logistic regression 
+"""
+def forward(X, W, b):
+    # softmax
+    a = X.dot(W) + b
+    expa = np.exp(a)
+    y = expa / expa.sum(axis=1, keepdims=True)
+    return y
+
+
+def predict(p_y):
+    return np.argmax(p_y, axis=1)
+
+
+def error_rate(p_y, t):
+    prediction = predict(p_y)
+    return np.mean(prediction != t)
+
+"""
+Cross Entropy Cost
+"""
+def cost(p_y, t):
+    tot = t * np.log(p_y)
+    return -tot.sum()
+
+"""
+Gradient of weight matrix
+"""
+def gradW(t, y, X):
+    return X.T.dot(t - y)
+
+"""
+Gradient of bias
+"""
+def gradb(t, y):
+    return (t - y).sum(axis=0)
+
+"""
+Function that creates an indicator matrix from y (one hot encoding) 
+"""
+def y2indicator(y):
+    N = len(y)
+    y = y.astype(np.int32)
+    ind = np.zeros((N, 10))
+    for i in range(N):
+        ind[i, y[i]] = 1
+    return ind
+
+"""
+Function to run the full benchmark
+"""
+def benchmark_full():
+    X, Y = get_normalized_data()
+
+    print("Performing logistic regression...")
+    # lr = LogisticRegression(solver='lbfgs')
+
+    # # test on the last 1000 points
+    # lr.fit(X[:-1000, :200], Y[:-1000]) # use only first 200 dimensions
+    # print lr.score(X[-1000:, :200], Y[-1000:])
+    # print "X:", X
+
+    # normalize X first
+    # mu = X.mean(axis=0)
+    # std = X.std(axis=0)
+    # X = (X - mu) / std
+
+    Xtrain = X[:-1000,]
+    Ytrain = Y[:-1000]
+    Xtest  = X[-1000:,]
+    Ytest  = Y[-1000:]
+
+    # convert Ytrain and Ytest to (N x K) matrices of indicator variables
+    N, D = Xtrain.shape
+    Ytrain_ind = y2indicator(Ytrain)
+    Ytest_ind = y2indicator(Ytest)
+
+    W = np.random.randn(D, 10) / np.sqrt(D)
+    b = np.zeros(10)
+    LL = []
+    LLtest = []
+    CRtest = []
+
+    # reg = 1
+    # learning rate 0.0001 is too high, 0.00005 is also too high
+    # 0.00003 / 2000 iterations => 0.363 error, -7630 cost
+    # 0.00004 / 1000 iterations => 0.295 error, -7902 cost
+    # 0.00004 / 2000 iterations => 0.321 error, -7528 cost
+
+    # reg = 0.1, still around 0.31 error
+    # reg = 0.01, still around 0.31 error
+    lr = 0.00004
+    reg = 0.01
+    for i in range(500):
+        p_y = forward(Xtrain, W, b)
+        # print "p_y:", p_y
+        ll = cost(p_y, Ytrain_ind)
+        LL.append(ll)
+
+        p_y_test = forward(Xtest, W, b)
+        lltest = cost(p_y_test, Ytest_ind)
+        LLtest.append(lltest)
+        
+        err = error_rate(p_y_test, Ytest)
+        CRtest.append(err)
+
+        W += lr*(gradW(Ytrain_ind, p_y, Xtrain) - reg*W)
+        b += lr*(gradb(Ytrain_ind, p_y) - reg*b)
+        if i % 50 == 0:
+            print("Cost at iteration %d: %.6f" % (i, ll))
+            print("Error rate:", err)
+
+    p_y = forward(Xtest, W, b)
+    print("Final error rate:", error_rate(p_y, Ytest))
+    iters = range(len(LL))
+    plt.plot(iters, LL, iters, LLtest)
+    plt.show()
+    plt.plot(CRtest)
+    plt.show()
+
+"""
+Function to run benchmark, after performing PCA
+"""
+def benchmark_pca():
+    X, Y, _, _ = get_transformed_data()
+    X = X[:, :300]
+
+    # normalize X first
+    mu = X.mean(axis=0)
+    std = X.std(axis=0)
+    X = (X - mu) / std
+
+    print("Performing logistic regression...")
+    Xtrain = X[:-1000,]
+    Ytrain = Y[:-1000]
+    Xtest  = X[-1000:,]
+    Ytest  = Y[-1000:]
+
+    N, D = Xtrain.shape
+    Ytrain_ind = np.zeros((N, 10))
+    for i in range(N):
+        Ytrain_ind[i, Ytrain[i]] = 1
+
+    Ntest = len(Ytest)
+    Ytest_ind = np.zeros((Ntest, 10))
+    for i in range(Ntest):
+        Ytest_ind[i, Ytest[i]] = 1
+
+    W = np.random.randn(D, 10) / np.sqrt(D)
+    b = np.zeros(10)
+    LL = []
+    LLtest = []
+    CRtest = []
+
+    # D = 300 -> error = 0.07
+    lr = 0.0001
+    reg = 0.01
+    for i in range(200):
+        p_y = forward(Xtrain, W, b)
+        # print "p_y:", p_y
+        ll = cost(p_y, Ytrain_ind)
+        LL.append(ll)
+
+        p_y_test = forward(Xtest, W, b)
+        lltest = cost(p_y_test, Ytest_ind)
+        LLtest.append(lltest)
+
+        err = error_rate(p_y_test, Ytest)
+        CRtest.append(err)
+
+        W += lr*(gradW(Ytrain_ind, p_y, Xtrain) - reg*W)
+        b += lr*(gradb(Ytrain_ind, p_y) - reg*b)
+        if i % 20 == 0:
+            print("Cost at iteration %d: %.6f" % (i, ll))
+            print("Error rate:", err)
+
+    p_y = forward(Xtest, W, b)
+    print("Final error rate:", error_rate(p_y, Ytest))
+    iters = range(len(LL))
+    plt.plot(iters, LL, iters, LLtest)
+    plt.show()
+    plt.plot(CRtest)
+    plt.show()
+
+
+if __name__ == '__main__':
+    benchmark_pca()
+    # benchmark_full()
+
