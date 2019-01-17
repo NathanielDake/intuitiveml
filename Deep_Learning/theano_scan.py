e441aadd1b7260a81bec95fbc106f3e324510553
Nate <ndake11@gmail.com>
2019-01-17 12:04:58 -0700

    rate recurrent unit in code, introduction and architecture of GRU

--- Deep_Learning/theano_scan.py
+++ Deep_Learning/theano_scan.py
@@ -1,6 +1,7 @@
 import numpy as np
 import theano
 import theano.tensor as T
+import matplotlib.pyplot as plt
 
 
 def scan_example_a():
@@ -27,12 +28,14 @@ def scan_example_a():
     print('Output: ', output_value)
 
 
-def recurrence(n, fn_1, fn_2):
-    return fn_1 + fn_2, fn_1
+
 
 
 def scan_example_b():
     N = T.iscalar('N')
+
+    def recurrence(n, fn_1, fn_2):
+        return fn_1 + fn_2, fn_1
 
     outputs, updates = theano.scan(
         fn=recurrence,
@@ -51,5 +54,60 @@ def scan_example_b():
     print('Output: ', output_value)
 
 
+def scan_example_c():
+    X = 2 * np.random.randn(300) + np.sin(np.linspace(0, 3*np.pi, 300))
+    plt.plot(X)
+    plt.title('Original')
+    plt.show()
+
+    decay = T.scalar('decay')
+    sequence = T.vector('sequence')
+
+    def recurrence(x, last, decay):
+        print(x, last, decay)
+        return (1 - decay)*x + decay*last
+
+    outputs, _ = theano.scan(
+        fn=recurrence,
+        sequences=sequence,
+        n_steps=sequence.shape[0],
+        outputs_info=[np.float64(0)],
+        non_sequences=[decay]
+    )
+
+    lpf = theano.function(
+        inputs=[sequence, decay],
+        outputs=outputs
+    )
+
+    Y = lpf(X, 0.99)
+    plt.plot(Y)
+    plt.title('Filtered')
+    plt.show()
+
+
+def scan_example_d():
+    sequence = T.vector('sequence')
+
+    def recurrence(current_x, current_total):
+        return current_x + current_total
+
+    outputs, _ = theano.scan(
+        fn=recurrence,
+        sequences=sequence,
+        outputs_info=[np.float64(0)]
+    )
+
+    calculate_sum = theano.function(
+        inputs=[sequence],
+        outputs=outputs
+    )
+
+    final_sum = calculate_sum(np.array([1,2,3,4]))
+
+    print(final_sum)
+
+
+
 if __name__ == '__main__':
-    scan_example_b()
+    scan_example_d()
