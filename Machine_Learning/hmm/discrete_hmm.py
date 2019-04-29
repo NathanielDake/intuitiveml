754747aef3ffb406b75aff1050be189816475c0e
Nate Dake <ndake11@gmail.com>
2019-04-29 08:41:52 -0600

    refactored HMM discrete class

--- Machine_Learning/hmm/discrete_hmm.py
+++ Machine_Learning/hmm/discrete_hmm.py
@@ -32,14 +32,12 @@ class HMM:
         self.pi = np.ones(self.M) / self.M
         self.A = random_normalized(self.M, self.M)
         self.B = random_normalized(self.M, V)
-        print("Initial A: ", self.A)
-        print("Initial B: ", self.B)
+        print("Initial A: \n", self.A)
+        print("Initial B: \n", self.B)
 
         # Enter main Expectation Maximization Loop
         costs = []
         for it in range(max_iter):
-            if it % 10 == 0:
-                print("it: ", it)
 
             # Lists for alpha and beta. Remember, learning updates variables for each
             # individual sequence, and they may be different lengths. Hence, we cannot
@@ -119,12 +117,53 @@ class HMM:
             self.A = a_num / den_a
             self.B = b_num / den_b
 
-        print("A: ", self.A)
-        print("B: ", self.B)
-        print("pi: ", self.pi)
+        print("A learned from training: \n", self.A)
+        print("B learned from training: \n", self.B)
+        print("pi learned from training: \n", self.pi)
 
         plt.plot(costs)
+        plt.xlabel("Iteration Number")
+        plt.ylabel("Cost")
         plt.show()
+
+    def likelihood(self, x):
+        """ Forward algorithm. Likelihood function for one observation. Returns P(x | model). """
+        T = len(x)
+        alpha = np.zeros((T, self.M))
+        alpha[0] = self.pi * self.B[:, x[0]]
+        for t in range(1, T):
+            alpha[t] = alpha[t-1].dot(self.A) * self.B[:, x[t]]
+
+        return alpha[-1].sum()
+
+    def likelihood_multi(self, X):
+        """Calculates the likelihoods of all observations."""
+        return np.array([self.likelihood(x) for x in X])
+
+    def log_likelihood_multi(self, X):
+        """Returns log likelihood of every observation."""
+        return np.log(self.likelihood_multi(X))
+
+    def get_state_sequence(self, x):
+        """Viterbi Algorithm. Returns the most likely hidden state sequence given observed sequence X."""
+        T = len(x)
+        delta = np.zeros((T, self.M))
+        psi = np.zeros((T, self.M))
+        delta[0] = self.pi * self.B[:, x[0]]
+
+        # Loop through every other time, loop through all states
+        for t in range(1, T):
+            for j in range(self.M):
+                delta[t, j] = np.max(delta[t-1]*self.A[:, j] * self.B[j, x[t]])
+                psi[t, j] = np.argmax(delta[t-1]*self.A[:, j])
+
+        # Backtrack
+        states = np.zeros(T, dtype=np.int32)
+        states[T-1] = np.argmax(delta[T-1])
+        for t in range(T-2, -1, -1):
+            states[t] = psi[t+1, states[t+1]]
+        return states
+
 
 def fit_coin(file_key):
     """Loads data and trains HMM."""
@@ -137,9 +176,25 @@ def fit_coin(file_key):
     # Instantiate object of class HMM with 2 hidden states (heads and tails)
     hmm = HMM(2)
     hmm.fit(X)
+    L = hmm.log_likelihood_multi(X).sum()
+    print("Log likelihood with fitted params: ", round(L, 3))
 
+    # Try the true values
+    hmm.pi = np.array([0.5, 0.5])
+    hmm.A = np.array([
+        [0.1, 0.9],
+        [0.8, 0.2]
+    ])
+    hmm.B = np.array([
+        [0.6, 0.4],
+        [0.3, 0.7]
+    ])
+    L = hmm.log_likelihood_multi(X).sum()
+    print("Log Likelihood with true params: ", round(L, 3))
 
-
+    # Viterbi
+    print("Best state sequence:\n", str(X[0]).replace(",",""))
+    print("", hmm.get_state_sequence(X[0]))
 
 
 if __name__ == "__main__":
