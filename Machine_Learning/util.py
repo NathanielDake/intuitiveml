0926ff29dbecb765fb739bc9d30f7430edf8e5dc
Nate Dake <ndake11@gmail.com>
2019-10-22 16:27:07 -0600

    started customer segmentation cluster analysis

--- Machine_Learning/util.py
+++ Machine_Learning/util.py
@@ -1,12 +1,37 @@
 from __future__ import print_function, division
 from builtins import range, input
-# Note: you may need to update your version of future
-# sudo pip install -U future
-
 
 import numpy as np
+import pandas
+import boto3
 import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+
+
+s3 = boto3.client('s3')
+
+
+def get_csv_from_s3(key, bucket="intuitiveml-data-sets"):
+    obj = s3.get_object(Bucket=bucket, Key=key)
+    return pandas.read_csv(obj["Body"])
+
+
+def get_obj_s3(key, bucket="intuitiveml-data-sets"):
+    obj = s3.get_object(Bucket=bucket, Key=key)["Body"]
+    return obj
+
+
+def get_mnist_data(limit=None):
+    print("Reading and Transforming MNIST Data...")
+    df = get_csv_from_s3("mnist_train.csv")
+    data = df.values
+    np.random.shuffle(data)
+    X = data[:, 1:] / 255.0
+    Y = data[:, 0]
+    if limit is not None:
+        X, Y = X[:limit], Y[:limit]
+    return X, Y
+
 
 def plot_decision_boundary(X, model):
   h = .02  # step size in the mesh
