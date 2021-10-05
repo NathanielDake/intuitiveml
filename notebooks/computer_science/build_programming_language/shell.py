289e11b65d4faf23cbe72bb0fc8eff36ac9b4e23
Nathaniel Dake <squid@unsupervised.com>
2021-10-05 08:12:07 -0400

    ton of notes

---
+++ notebooks/computer_science/build_programming_language/shell.py
@@ -0,0 +1,11 @@
+import basic
+
+
+while True:
+    text = input('basic > ')
+    result, error = basic.run('<stdin>', text)
+
+    if error:
+        print(error.as_string())
+    else:
+        print(result)
